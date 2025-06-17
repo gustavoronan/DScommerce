467cc4104e6a4ed278babb4c1473288ec54c5f1a
Gustavo Ronan <gustavoronn@gmail.com>
2025-06-17 20:10:54 -0300

    add: funcionalidade carrinho dinamico

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -6,33 +6,42 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../models/product";
 import * as productService from "../../services/product-service";
+import * as cartService from "../../services/cart-service";
 
 export default function ProductDetails() {
   const params = useParams();
   const [product, setProduct] = useState<ProductDTO>();
   const navigate = useNavigate();
 
-  useEffect(()=> {
-    productService.findById(Number(params.productId))
-    .then(response => {
-      setProduct(response.data);
-    })
-    .catch(()=>{
-       navigate("/catalog")
-    });
-   
-  },[])
+  useEffect(() => {
+    productService
+      .findById(Number(params.productId))
+      .then((response) => {
+        setProduct(response.data);
+      })
+      .catch(() => {
+        navigate("/catalog");
+      });
+  }, []);
 
+  function handleByClick() {
+    if (product) {
+      cartService.addProduct(product);
+      navigate("/cart")
+    }
+  }
 
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
-        {product &&
-         <ProductDetailsCard productProps={product}></ProductDetailsCard>
-        }
+        {product && (
+          <ProductDetailsCard productProps={product}></ProductDetailsCard>
+        )}
 
         <div className="dsc-btn-page-container">
-          <ButtonPrimary name="Comprar"></ButtonPrimary>
+          <div onClick={handleByClick}>
+            <ButtonPrimary name="Comprar"></ButtonPrimary>
+          </div>
           <Link to="/">
             <ButtonInverse name="Início"></ButtonInverse>
           </Link>
